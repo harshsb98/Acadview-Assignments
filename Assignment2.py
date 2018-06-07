2e56d56dffda8f64e637a7e39023e7109252e6c5
harshsb98 <harshsb98@gmail.com>
2018-06-07 08:36:10 +0530

    My second Assignment

---
+++ Assignment2.py
@@ -0,0 +1,24 @@
+#Q1
+print("abc")
+
+#Q2
+print("Acad"+"View")
+
+#Q3
+a=1
+b=2
+c=3
+print(a ,b, c)
+#Q4
+print("Let\'s Get Started")
+
+#Q5
+s="Acadview"
+course="Python"
+fees=5000
+print("hi,%s your course:%s your fees:%d"%(s,course,fees))
+
+#Q6
+name="Tony Stark"
+salary=1000000
+print(('%s''%d')%(name,salary))

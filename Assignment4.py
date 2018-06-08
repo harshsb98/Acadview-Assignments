b012249d2c16b0cdf9302ee7242afafb07790133
harshsb98 <harshsb98@gmail.com>
2018-06-08 21:30:22 +0530

    My 4th assignment

---
+++ Assignment4.py
@@ -0,0 +1,38 @@
+#Q1
+#a=("abc",1,2,3.4)
+#print(len(a))
+
+#Q2
+#a=(4,12,1,33,4)
+#print(max(a),min(a))
+
+#Q3
+#a=(2*4*9)
+#print(a)
+
+#Q4
+#s1=set([1,2,3,4,5])
+#s2=set([3,4,5])
+#print(s1-s2,s1<=s2,s1>=s2,s1&s2)
+
+#Q5
+#a=input("enter name")
+#b=input("enter marks")
+#c=input("enter name")
+#d=input("enter marks")
+#e=input("enter name")
+#f=input("enter marks")
+#g=input("enter name")
+#h=input("enter marks")
+#D={a:b,c:d,e:f,g:h}
+#print(D)
+
+#Q7
+l=["M","I","S","S","I","S","S","I","P","P","I"]
+m=l.count("M")
+i=l.count("I")
+s=l.count("S")
+p=l.count("P")
+D={'m':m,'i':i,'s':s,'p':p}
+print(D)
+

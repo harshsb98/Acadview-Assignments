f69dee22e3bcaeefd37e7df2f9046d6ac46a1f87
harshsb98 <harshsb98@gmail.com>
2018-06-11 18:33:59 +0530

    5th assignment

---
+++ Assignment5.py
@@ -0,0 +1,53 @@
+#Q1
+#y=int(input("enter a year"))
+#if(y%4==0):
+ #   print("leap year")
+
+#Q2
+#l=int(input("side1"))
+#b=int(input("side2"))
+#if(l==b):
+#    print("square");
+#else:
+#    print("rectangle")
+
+#Q3
+#a=int(input("enter age of 1st person"))
+#b=int(input("Enter age of 2nd person"))
+#c=int(input("Enter age of 3rd person"))
+#if(a>b):
+#    if(a>c):
+#        print("a is oldest")
+#    else:
+#        print("c is oldest")
+#    if(b<c):
+#        print("b is youngest")
+#    else:
+#        print("c is youngest")
+#else:
+#    if(b>c):
+#        print("b is oldest")
+#    else:
+#        print("c is oldest")
+#    if(a<c):
+#        print("a is youngest")
+#    else:
+#        print("c is youngest")
+
+#Q4
+#x=int(input("enter prize"))
+#if(x>=1&x<=50):
+#    print("No prize")
+#elif(x>=51&x<=150):
+#    print("wooden Dog")
+#elif(x>=151&x<=180):
+#    print("Book")
+#else:
+#    print("chocolates")
+
+#Q5
+#x=int(input("enter units"))
+#x=x*100
+#if(x>1000):
+#    x=x*100-x*0.1*100
+#print(x)
